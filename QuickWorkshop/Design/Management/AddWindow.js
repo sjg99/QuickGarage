71f20d1060611a35462b3e73c6836f31fbf28c1c
sjg99 <38005341+sjg99@users.noreply.github.com>
2018-09-28 09:32:33 -0500

    Management Design

--- QuickWorkshop/Design/Management/AddWindow.js
+++ QuickWorkshop/Design/Management/AddWindow.js
@@ -1,11 +1,10 @@
-$('.iconos .cajaadd').click(function(){  
-  $('.VentanaAdd').css('top', '50%');
-});
-$('.edit').click(function () {
-    $('.VentanaAdd').css('top', '50%');
+$('.iconos .cajaadd, .edit').click(function(){  
+  $('.VentanaAdd').css('display', 'flex');
+  $('nav, .infoBar, .contenedor-padre').css('filter', 'blur(2px)'); 
 });
 $('html').click(function(){
-  $('.VentanaAdd').css('top', '-50%');
+  $('.VentanaAdd').css('display', 'none');
+  $('nav, .infoBar, .contenedor-padre').css('filter', 'blur(0px)');  
 });
 $('.iconos .cajaadd, .VentanaAdd, .edit').click(function(e){
   e.stopPropagation();
