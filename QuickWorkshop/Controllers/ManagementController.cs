6e769d4d6a6ab6b2b17828c683266a62b7e44575
sjg99 <38005341+sjg99@users.noreply.github.com>
2018-09-29 20:50:45 -0500

    Mangement Asp.Net Initial

--- QuickWorkshop/Controllers/ManagementController.cs
+++ QuickWorkshop/Controllers/ManagementController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QuickWorkshop.ViewModels;
 
 namespace QuickWorkshop.Controllers
 {
     public class ManagementController : Controller
     {
         // GET: Management
+        private ManagementLoading managementLoading()
+        {
+            ManagementLoading ml = new ManagementLoading();
+            return ml;
+        }
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult GetProducts()
+        {
+            return View("Index", managementLoading());
+        }
+        public ActionResult GetServices()
+        {
+            return View("IndexSer", managementLoading());
+        }
+        public ActionResult GetUsers()
+        {
+            return View("IndexUs", managementLoading());
+        }
+        public ActionResult GetOrders()
+        {
+            return View("IndexOrd", managementLoading());
+        }
+
+
     }
 }
---
+++ QuickWorkshop/ViewModels/ManagementLoading.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QuickWorkshop.Models;
+
+namespace QuickWorkshop.ViewModels
+{
+    public class ManagementLoading
+    {        
+        public List<product> products = new List<product>();
+        public List<service> services = new List<service>();
+        public List<user> users = new List<user>();
+        public List<order> orders = new List<order>();
+
+        public ManagementLoading()
+        {
+            using (QWDBEntities db = new QWDBEntities())
+            {
+                var GetProducts = db.products.Where(x => x.ProductId > 0);
+                var GetServices = db.services.Where(x => x.ServiceID > 0);
+                var GetUsers = db.users.Where(x => x.Status == "Activo");
+                var GetOrders = db.orders.Where(x => x.OrderId > 0);
+                foreach (var r in GetProducts)
+                {
+                    products.Add(r);
+                }
+                foreach (var r in GetServices)
+                {
+                    services.Add(r);
+                }
+                foreach(var r in GetUsers)
+                {
+                    users.Add(r);
+                }
+                foreach(var r in GetOrders)
+                {
+                    orders.Add(r);
+                }
+            }
+        }
+    }
+}
