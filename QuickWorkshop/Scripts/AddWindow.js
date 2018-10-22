7b64fd11b3a34491d59b70ebbb9a82484745745b
sjg99 <38005341+sjg99@users.noreply.github.com>
2018-10-22 11:46:14 -0500

    Management Responsive scss

--- QuickWorkshop/Scripts/AddWindow.js
+++ QuickWorkshop/Scripts/AddWindow.js
@@ -1,9 +1,9 @@
 $('.iconos .cajaadd, .edit').click(function(){  
-  $('.VentanaAdd').css('display', 'flex');
+  $('.Ventana').css('display', 'block');
   $('nav, .infoBar, .contenedor-padre').css('filter', 'blur(2px)'); 
 });
 $('html').click(function(){
-  $('.VentanaAdd').css('display', 'none');
+  $('.Ventana').css('display', 'none');
   $('nav, .infoBar, .contenedor-padre').css('filter', 'blur(0px)');  
 });
 $('.iconos .cajaadd, .VentanaAdd, .edit').click(function(e){
